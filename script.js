90b79901b9334a1f1793819f6adcdc26296e55c9
mateusmaciel460 <mateusmaciel460@gmail.com>
2025-04-03 17:14:24 -0300

    commit de fechamento do projeto

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-let texto = document.querySelector('p');
-texto.textContent = `${location.origin}/projeto-base/style.css`;
+let elementoProjetos = document.querySelector('#elemento-projetos');
+const listaProjetos = [
+    { id: 1, titulo: 'Projeto base', caminho: 'projeto-base' },
+    { id: 2, titulo: 'Número secreto', caminho: 'numero-secreto' },
+    { id: 3, titulo: 'Sorteador de números', caminho: 'sorteador-numeros' },
+    { id: 4, titulo: 'Jogos', caminho: 'jogos' },
+    { id: 5, titulo: 'Carrinho de compras', caminho: 'carrinho-compras' },
+    { id: 6, titulo: 'Cine - entrada', caminho: 'cine-entrada' },
+    { id: 7, titulo: 'Amigo secreto', caminho: 'amigo-secreto' },
+];
 
-console.log(location.origin);
+listaProjetos.forEach((projeto) => {
+    elementoProjetos.innerHTML += `
+        <a 
+            href="${projeto.caminho}/" 
+            target="_blank" class="conteudo__texto">${projeto.titulo}</a>
+    `;
+});
