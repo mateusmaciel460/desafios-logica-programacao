150c1948e1eacee4bbeaa91c2e6d80026e04319a
mateusmaciel460 <mateusmaciel460@gmail.com>
2025-04-18 18:17:12 -0300

    adicionando funcionalidade dinâmica em cine-entrada

--- cine-entrada/script.js
+++ cine-entrada/script.js
@@ -1,113 +1,102 @@
-let elementoMensagemErro = document.querySelector('#elemento-mensagem-erro');
-let elementoEntradasDisponiveis = document.querySelector('#elemento-entradas-disponiveis');
-let elementoTipo = document.querySelector('#tipo');
+let listaCadeirasReservadasPorPessoa = [];
+let listaCompradores = [];
 
-let listaEntradasDisponiveis = [
-    { titulo: 'Setor superior', quantidade: 0, quantidadeInicial: 0 },
-    { titulo: 'Setor central', quantidade: 0, quantidadeInicial: 0 },
-    { titulo: 'Setor inferior', quantidade: 0, quantidadeInicial: 0 }
-];
+let elementoCadeirasDisponiveis = document.querySelector('#elemento-cadeiras-disponiveis');
+let elementoPrecoCompra = document.querySelector('#elemento-preco-compra');
+let elementoPrecoIngresso = document.querySelector('#elemento-preco-ingresso');
+let elementoLugaresReservados = document.querySelector('#elemento-lugares-reservados');
+let elementoCompradores = document.querySelector('#elemento-compradores');
 
-function escolher() {
-    let quantidade = document.querySelector('#quantidade').value;
-    let tipo = document.querySelector('#tipo').value;
+let precoIngresso = 23;
+let precoCompra = 0;
+let quantidadeAssentos = 22;
+let quantidadeCadeirasReservadas = 0;
 
-    elementoMensagemErro.innerHTML = '';
+function reservarCadeira(numeracao) {
+    let cadeiraReservada = document.querySelector(`#cadeira-${numeracao}`);
+    let classeCadeiraReservada = cadeiraReservada.classList;
 
-    // Validação
-    if ((quantidade && tipo) == '') {
-        exibirMensagemErro('Preencha todos os campos!');
-        return;
-    }
+    // Modificando as cores das poltronas
+    classeCadeiraReservada.remove('conteudo__vermelho');
+    classeCadeiraReservada.add('conteudo__verde');
+    cadeiraReservada.removeAttribute('onclick');
 
-    if (quantidade <= 0) {
-        exibirMensagemErro(`A quantidade deve ser um número maior que 0, e não [${quantidade}]`);
-        return;
-    }
+    // Adicionando cadeiras reservadas
+    listaCadeirasReservadasPorPessoa.push(numeracao);
+    let totalCadeirasReservadas = listaCadeirasReservadasPorPessoa.length;
 
-    document.querySelector('#quantidade').value = '';
+    // Mostrar valores
+    precoCompra += precoIngresso;
+    elementoPrecoCompra.textContent = `${precoCompra} (${totalCadeirasReservadas}x)`;
 
-    elementoEntradasDisponiveis.innerHTML = '';
-    listaEntradasDisponiveis.forEach((entrada) => {
-        if (entrada.titulo == tipo) {
-            let palavraCadeira = quantidade > 1 ? 'cadeiras disponíveis' : 'cadeira disponível';
+    // Mostrar lugares reservados
+    elementoLugaresReservados.textContent = `${listaCadeirasReservadasPorPessoa.join(', ')}`;
 
-            if (quantidade > entrada.quantidade) {
-                exibirMensagemErro(`Não temos ${quantidade} ${palavraCadeira}, disponibilidade: ${entrada.quantidade}.`);
-            } else {
-                entrada.quantidade -= quantidade;
-            }
-        }
+    document.querySelector('#botao-compra').removeAttribute('disabled');
+}
 
-        elementoEntradasDisponiveis.innerHTML += `
-            <li class="conteudo__bloco">
-                ${entrada.titulo}:
-                <span class="conteudo__destaque">${entrada.quantidade}</span>
-            </li>
-        `;
+function comprar() {
+    let nomeComprador = document.querySelector('#nome').value;
+    let lugaresComprador = listaCadeirasReservadasPorPessoa;
+    let totalCadeirasReservadas = listaCadeirasReservadasPorPessoa.length;
+    let cadeiraEspecifica;
+
+    listaCompradores.push({ 
+        nome: nomeComprador, lugares: lugaresComprador, quantidade: totalCadeirasReservadas
     });
 
-    let somaGeralEntradas = somaListaEntrada();
+    exibirCompradoresNaTela(nomeComprador, lugaresComprador, totalCadeirasReservadas);
 
-    if (somaGeralEntradas[0] == 0) {
-        document.querySelector('#botao-reiniciar').removeAttribute('disabled');
-    }
-}
+    listaCadeirasReservadasPorPessoa.forEach((reservada) => {
+        cadeiraEspecifica = document.querySelector(`#cadeira-${reservada}`);
 
-function reiniciar() {
-    document.querySelector('#botao-reiniciar').setAttribute('disabled', true);
-    document.querySelector('#quantidade').value = '';
-    elementoEntradasDisponiveis.innerHTML = '';
-    elementoMensagemErro.innerHTML = '';
-    exibirEntradasInicial();
-}
-
-function exibirTiposNaTela() {
-    listaEntradasDisponiveis.forEach((entrada) => {
-        elementoTipo.innerHTML += `
-            <option value="${entrada.titulo}">${entrada.titulo}</option>`;
-    });
-}
-
-function adicionarEntradasIniciais() {
-    listaEntradasDisponiveis.forEach((entrada) => {
-        entrada.quantidade = 100;
-        entrada.quantidadeInicial = 100;
-    });
-}
-
-function exibirEntradasInicial() {
-    adicionarEntradasIniciais();
-
-    listaEntradasDisponiveis.forEach((entrada) => {
-        elementoEntradasDisponiveis.innerHTML += `
-            <li class="conteudo__bloco">
-                ${entrada.titulo}:
-                <span class="conteudo__destaque">${entrada.quantidade}</span>
-            </li>`;
-    });
-}
-
-function somaListaEntrada() {
-    let somaQuantidade = 0;
-    let somaQuantidadeInicial = 0;
-    let listaEntradasSomadas = [];
-
-    listaEntradasDisponiveis.forEach((entrada) => {
-        somaQuantidade += entrada.quantidade;
-        somaQuantidadeInicial += entrada.quantidadeInicial;
+        cadeiraEspecifica.classList.remove('conteudo__verde');
+        cadeiraEspecifica.classList.add('conteudo__cinza');
+        cadeiraEspecifica.removeAttribute('onclick');
+        cadeiraEspecifica.title = nomeComprador;
     });
 
-    listaEntradasSomadas.push(somaQuantidade, somaQuantidadeInicial);
-
-    return listaEntradasSomadas;
+    resetarPersonalidadesDaTela();
+    quantidadeCadeirasReservadas += totalCadeirasReservadas;
+    aparecerReiniciar();
 }
 
-function exibirMensagemErro(conteudo) {
-    elementoMensagemErro.innerHTML += `
-        <span class="conteudo__mensagem--alerta">${conteudo}</span>
+function resetarPersonalidadesDaTela() {
+    elementoPrecoCompra.textContent = 0;
+    elementoLugaresReservados.textContent = 'Nenhum';
+    document.querySelector('#nome').value = '';
+    listaCadeirasReservadasPorPessoa = [];
+    precoCompra = 0;
+}
+
+function exibirCompradoresNaTela(comprador, lugares, quantidadeLugares) {
+    elementoCompradores.innerHTML += `
+        <p class="conteudo__texto borda__superior">
+            Comprador: <span class="conteudo__destaque">${comprador}</span> |
+            Lugares: <span class="conteudo__destaque">${lugares.join(', ')}</span> |
+            Quantidade: <span class="conteudo__destaque">${quantidadeLugares}</span>
+        </p>
     `;
 }
 
-exibirEntradasInicial();
-exibirTiposNaTela();
+function exibirCadeirasDisponiveisNaTela() {
+    for (let contador = 1; contador <= quantidadeAssentos; contador++) {
+        elementoCadeirasDisponiveis.innerHTML += `
+            <div class="cadeiras__disponiveis conteudo__bloco conteudo__texto conteudo__vermelho" id="cadeira-${contador}" onclick="reservarCadeira(${contador})">
+                    0${contador}
+            </div>
+        `;
+    }
+
+    elementoPrecoIngresso.textContent = precoIngresso;
+}
+
+function aparecerReiniciar() {
+    if (quantidadeAssentos == quantidadeCadeirasReservadas) {
+        document.querySelector('#botao-reiniciar').removeAttribute('disabled');
+    }
+
+    console.log(quantidadeAssentos, quantidadeCadeirasReservadas);
+}
+
+exibirCadeirasDisponiveisNaTela();
