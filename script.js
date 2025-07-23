c4a35bdbaed6592b1668cccbdab81f85e9bee73e
mateusmaciel460 <mateusmaciel460@gmail.com>
2025-07-23 16:43:57 -0300

    atualizando pagina-inicial do projeto

---
+++ script.js
@@ -0,0 +1,23 @@
+const elementoProjetos = document.querySelector('#elementos-projetos');
+const listaProjetos = [
+    { id: 1, titulo: "Projeto Base" }, 
+    { id: 2, titulo: "Número Secreto" },
+    { id: 3, titulo: "Sorteador Números" }
+];
+
+function exibirProjetosNaTela() {
+    listaProjetos.forEach((projeto) => {
+        elementoProjetos.innerHTML += `
+            <a href="${retornarTagProjeto(projeto.titulo)}/index.html" target="_blank" class="conteudo__botao modelo__cor-azul" id="projeto-${projeto.id}">
+                ${projeto.titulo}
+            </a>
+        `;
+    });
+}
+
+function retornarTagProjeto(texto) {
+    return texto.normalize('NFD').
+        replace(/[\u0300-\u036f]/g, '').toLowerCase().replace(" ", "-");
+}
+
+exibirProjetosNaTela();
